ba13d60751b5d1ae913a409836d7263a6d4290aa
Hachiney <113271303+Hachiney@users.noreply.github.com>
2022-10-31 18:30:10 -0400

    Music first commited files

---
+++ components/Loader.jsx
@@ -0,0 +1,17 @@
+import { loader } from '../assets';
+
+
+
+
+const Loader = ({title}) => (
+  <div className='w-full flex justify-center items-center
+  flex-col'>
+    <img src={loader} alt='loader' className='w-32 h-32
+    object-container' />
+    <h1 className='font-bold text-2xl text-white mt-2'>
+      {title || 'Loading...'} 
+    </h1>
+  </div>
+);
+
+export default Loader;
